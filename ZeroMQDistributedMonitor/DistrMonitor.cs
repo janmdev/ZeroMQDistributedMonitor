417bd798e25492445a0d2ed1f4221908b1f271d8
janmdev <jan.majewski@student.put.poznan.pl>
2022-09-27 13:22:19 +0200

    poller

--- ZeroMQDistributedMonitor/DistrMonitor.cs
+++ ZeroMQDistributedMonitor/DistrMonitor.cs
@@ -7,7 +7,6 @@ namespace ZeroMQDistributedMonitor
 {
     public class DistrMonitor<T> : IDisposable
     {
-        //private readonly string _objTopic = "obj";
         private readonly string _lockTopic = "lock";
         public DistrMonitor(string pubAddress, IEnumerable<string> subAddresses, bool initLock = true)
         {
@@ -20,16 +19,17 @@ namespace ZeroMQDistributedMonitor
             foreach(var item in subAddresses)
             {
                 subSocket.Connect($"tcp://{item}");
-                //subSocket.Subscribe(_objTopic);
                 subSocket.Subscribe(_lockTopic);
             }
-            Task.Run(receiveMessages);
+            poller = new NetMQPoller { subSocket };
+            subSocket.ReceiveReady += receiveMessages;
+            poller.RunAsync();
         }
 
         private bool locked;
         private bool interLocked;
         private T distributedObject { get; set; }
-
+        private NetMQPoller poller;
         private PublisherSocket pubSocket;
         private SubscriberSocket subSocket;
 
@@ -85,39 +85,28 @@ namespace ZeroMQDistributedMonitor
             interLocked = false;
         }
 
-        private Task receiveMessages()
+        private void receiveMessages(object? sender, NetMQSocketEventArgs args)
         {
-            while(true)
+            string topic = args.Socket.ReceiveFrameString();
+            lock(this)
             {
-                string topic = subSocket.ReceiveFrameString();
-                lock(this)
+                if (topic == _lockTopic)
                 {
-                    if (topic == _lockTopic)
+                    byte[] receivedObj = args.Socket.ReceiveFrameBytes();
+                    var lockObj = MessagePackSerializer.Deserialize<Lock<T>>(receivedObj);
+                    if (!lockObj.IsLocked)
                     {
-                        byte[] receivedObj = subSocket.ReceiveFrameBytes();
-                        var lockObj = MessagePackSerializer.Deserialize<Lock<T>>(receivedObj);
-                        if (!lockObj.IsLocked)
-                        {
-                            if (interLocked) Console.WriteLine("INTERLOCKED");
-                            locked = false;
-                            distributedObject = lockObj.Value;
-                            Monitor.Pulse(this);
-                            //Console.WriteLine("unlock");
-                        }
-                        if (lockObj.IsLocked)
-                        {
-                            if (interLocked) Console.WriteLine("INTERLOCKED");
-                            locked = true;
-                            //Console.WriteLine("lock");
-                        }
+                        locked = false;
+                        distributedObject = lockObj.Value;
+                        Monitor.Pulse(this);
+                        //Console.WriteLine("unlock");
+                    }
+                    if (lockObj.IsLocked)
+                    {
+                        locked = true;
+                        //Console.WriteLine("lock");
                     }
                 }
-                //if(topic == _objTopic)
-                //{
-                //    byte[] receivedObj = subSocket.ReceiveFrameBytes();
-                //    if (!interLocked)
-                //        distributedObject = MessagePackSerializer.Deserialize<T>(receivedObj);
-                //}
             }
         }
 
